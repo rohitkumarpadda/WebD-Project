64493ed699570c6712f2f2bb6938ec2ac16f8897
Padda Rohit Kumar <rohitkumarsoftware@gmail.com>
2024-04-02 04:12:53 +0530

    Refactor foundItemSchema in foundItem.js

--- models/foundItem.js
+++ models/foundItem.js
@@ -3,14 +3,38 @@ const Schema = mongoose.Schema;
 
 const foundItemSchema = new Schema(
   {
-    name: { type: String, required: true },
-    contactNo: { type: Number, required: true },
-    category: { type: String, required: true },
-    item: { type: String, required: true },
-    dateFound: { type: Date, required: true },
-    description: { type: String, required: true },
-    image: { type: String, required: true },
-    userEmail: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
+    contactNo: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    item: {
+      type: String,
+      required: true,
+    },
+    dateFound: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
   },
   { collection: "FoundItems" }
 );
