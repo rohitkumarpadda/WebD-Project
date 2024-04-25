915d8362b008d2c06c455ee0f29792c37db00b4b
Padda Rohit Kumar <rohitkumarsoftware@gmail.com>
2024-04-25 07:26:10 +0530

    Update session cookie settings in index.js

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true },
-    cookie: { maxAge: 1000 * 60 * 30 },
+    cookie: { secure: true, httpOnly: true, maxAge: 1000 * 60 * 30 },
   })
 );
 
